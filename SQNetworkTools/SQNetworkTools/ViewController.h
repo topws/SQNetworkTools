8fae4ddc1245fff06a2f4eac8fa410fd99a3e25c
钱卫 <634794819@qq.com>
2016-04-23 22:22:27 +0800

    1、创建项目

---
+++ SQNetworkTools/SQNetworkTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQNetworkTools
+//
+//  Created by 钱卫 on 16/4/23.
+//  Copyright © 2016年 钱卫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SQNetworkTools/SQNetworkTools/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SQNetworkTools
+//
+//  Created by 钱卫 on 16/4/23.
+//  Copyright © 2016年 钱卫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
